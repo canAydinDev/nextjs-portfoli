2569ef1f2f524dd7222c0ab9294d737f25b6117b
canaydindev <canaydindev@gmail.com>
2024-09-18 22:08:36 +0300

    last

---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+import ProjectsBtn from "../components/ProjectsBtn";
+import { fadeIn } from "../variants";
+import Avatar from "../components/Avatar";
+import ParticlesContainer from "../components/ParticlesContainer";
+
+const Home = () => {
+  return (
+    <div className="bg-primary/60 h-full">
+      <div className="w-full h-full bg-gradient-to-tr from-primary/10 via-black/30">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            Bridging Data, AI, <br />
+            and Development for{" "}
+            <span className="text-accent">Future-Ready Solutions</span>
+          </motion.h1>
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            We combine the power of data science, artificial intelligence, and
+            web development to deliver innovative, future-proof solutions. By
+            integrating advanced analytics, machine learning, and seamless web
+            experiences, we transform your digital vision into impactful,
+            scalable products that drive success.
+          </motion.p>
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+      </div>
+      <div>
+        <ParticlesContainer />
+      </div>
+      <motion.div
+        variants={fadeIn("down", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        transition={{ duration: 1, ease: "easeInOut" }}
+        className="w-full h-full max-w-[637px] max-h-[578px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+      >
+        <Avatar />
+      </motion.div>
+    </div>
+  );
+};
+
+export default Home;
