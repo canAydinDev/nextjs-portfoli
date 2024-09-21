a6363f6712991c42df844a686ff74269c8a18b97
canaydindev <canaydindev@gmail.com>
2024-09-21 19:02:10 +0300

    version-about

--- pages/about/index.js
+++ pages/about/index.js
@@ -141,7 +141,17 @@ const About = () => {
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={6} duration={5} />
                 </div>
-                <div>Years of Experience</div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]  ">
+                  Years of Experience
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={250} duration={5} />
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]  ">
+                  Satisfied clients
+                </div>
               </div>
             </div>
           </div>
