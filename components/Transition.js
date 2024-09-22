ebcf23b245e1b9a297713fbb89fe10b852483b38
canaydindev <canaydindev@gmail.com>
2024-09-22 21:37:07 +0300

    last version

--- components/Transition.js
+++ components/Transition.js
@@ -24,7 +24,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2, duration: 0.4, ease: "easeInOut" }}
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
       ></motion.div>
 
       <motion.div
@@ -33,7 +33,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.3, duration: 0.4, ease: "easeInOut" }}
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
       ></motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
@@ -41,7 +41,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.4, duration: 0.4, ease: "easeInOut" }}
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
       ></motion.div>
     </>
   );
